de78bac46c4e9a22dab02a3cf87615301a170b2a
a0xnirudh <anirudhanand722@gmail.com>
2015-08-16 13:44:17 +0530

    Modified message contact options

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "mail/contact_me.php",
+                url: "https://formspree.io/contact@init-lab.com",
                 type: "POST",
                 data: {
                     name: name,
